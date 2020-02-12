e0562eefcefa227a5a1f9c30526792a494fe3fb7
JUANDATM <kira00711@hotmail.com>
2020-02-12 16:59:42 -0600

    Data oK

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,18 +9,32 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DataComponent {
   forma: FormGroup;
+  usuario: object = {
+    nombrecompleto: {
+      nombre: 'juan',
+      apellido: 'torres'
+    },
+    correo: 'JUANTM@gmail.com'
+
+  }
 
   constructor() {
 
+
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('Juan'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required)
+      }),
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
+    this.forma.setValue(this.usuario);
   }
 
   guardarCambios() {
     console.log(this.forma.value);
+    this.forma.reset(this.usuario);
   }
 
 
